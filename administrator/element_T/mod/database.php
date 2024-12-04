9cc380893f9d73e2d423dbdcfd170a29b8794f40
YukioKitazaki <voduthanhtu@gmail.com>
2024-12-04 16:16:04 +0700

    sửa lại chữ word trong password

--- administrator/element_T/mod/database.php
+++ administrator/element_T/mod/database.php
@@ -6,11 +6,10 @@ class Database{
         $servername = $init["servername"];
         $dbname = $init["dbname"];
         $username = $init["username"];
-        $password = $init["pass"];
+        $password = $init["password"];
         $opt = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_OBJ);
         $this->connect = new PDO(dsn: "mysql:host=$servername;dbname=$dbname;charset=utf8",$username,$password,$opt);
-        $this->connect = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password, $opt);
-
+       
     }
 }
 /*$init = parse_ini_file("config.ini");
